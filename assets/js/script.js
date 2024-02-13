3d6d9d8e45fe54accdc75e49564278a7af87f263
paloma <palomaschultze@gmail.com>
2024-02-13 16:31:32 -0300

    Primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// Carousel
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false
+    });
+});
